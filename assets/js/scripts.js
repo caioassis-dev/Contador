f132641e64252c6ffc0756697a59d7c57bb23a38
caioassis-dev <caiofreire_1@hotmail.com>
2022-06-13 21:16:42 -0300

    adicionando regra na função para trocar cor quando o numero ficar em negativo e positivo

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,9 +4,15 @@ var currentNumber = 0
 function increment() {
   currentNumber = currentNumber + 1
   currentNumberWrapper.innerHTML = currentNumber
+  if (currentNumber >= 0) {
+    document.getElementById('currentNumber').style.color = 'green'
+  }
 }
 
 function decrement() {
   currentNumber = currentNumber - 1
   currentNumberWrapper.innerHTML = currentNumber
+  if (currentNumber < 0) {
+    document.getElementById('currentNumber').style.color = '#B11'
+  }
 }
